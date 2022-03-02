227e8dbac1881730ff91c83bcb6fda03666ca525
fkerich <festus.kerich@cellulant.io>
2022-03-02 14:00:44 +0300

    initial commit

---
+++ src/test/java/com/francium/multitenant/MultitenantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francium.multitenant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitenantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
